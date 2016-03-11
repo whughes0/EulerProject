7538c22732d20adf6dfe00c289a72d998edbbba3
whughes0 <whughes0@gmail.com>
2016-03-10 20:26:49 -0500

    Update P7.java

--- euler7/src/P7.java
+++ euler7/src/P7.java
@@ -2,9 +2,8 @@ import components.sequence.Sequence;
 import components.sequence.Sequence3;
 
 /**
- * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.  
- * What is the 10 001st prime number?
- * 
+ * https://projecteuler.net/problem=7
+ *
  * @author William Hughes
  *
  */
