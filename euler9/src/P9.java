36d4d6f67eef4084d6dcefb30753bc4389ca7cc0
whughes0 <whughes0@gmail.com>
2016-03-10 20:22:13 -0500

    Update P9.java

--- euler9/src/P9.java
+++ euler9/src/P9.java
@@ -1,7 +1,7 @@
 /**
- * Put a short phrase describing the program here.
+ * https://projecteuler.net/problem=9
  *
- * @author Put your name here
+ * @author William Hughes
  *
  */
 public final class P9 {
